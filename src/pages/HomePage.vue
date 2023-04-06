980defa0afb0196ef1c9fd82e654a6c34257de59
Ryan Wittman <OfficiallyRy@outlook.com>
2023-04-06 11:06:28 -0600

    Animation God coming through

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,16 +2,24 @@
 <div class="container-fluid">
   <div class="row align-items-center bg-dark elevation-1">
     <div class="col-12 col-md-5">
-      <span class="fs-1 fancy">Ryan Wittman</span>
+      <div class="fancy">
+        <span class="fs-1">Ryan Wittman</span>
+      </div>
     </div>
     <div class="col-4 col-md-1 offset-md-4 text-center">
-      <a href="https://github.com/TheOneTrueRy" target="_blank" class="hover" title="Visit my GitHub profile."><span class="fs-5 underline text-light">GitHub</span></a>
+      <div class="social">
+        <a href="https://github.com/TheOneTrueRy" target="_blank" class="hover" title="Visit my GitHub profile."><span class="fs-5 underline text-light">GitHub</span></a>
+      </div>
     </div>
     <div class="col-4 col-md-1 text-center">
-      <a href="https://www.linkedin.com/in/theonetruery/" target="_blank" class="hover" title="Visit my LinkedIn profile."><span class="fs-5 underline text-light">LinkedIn</span></a>
+      <div class="social">
+        <a href="https://www.linkedin.com/in/theonetruery/" target="_blank" class="hover" title="Visit my LinkedIn profile."><span class="fs-5 underline text-light">LinkedIn</span></a>
+      </div>
     </div>
     <div class="col-4 col-md-1 text-center">
-      <a href="https://codeworks.prentus.co/p/ryan-wittman" target="_blank" class="hover" title="Visit my Prentus profile."><span class="fs-5 underline text-light">Prentus</span></a>
+      <div class="social">
+        <a href="https://codeworks.prentus.co/p/ryan-wittman" target="_blank" class="hover" title="Visit my Prentus profile."><span class="fs-5 underline text-light">Prentus</span></a>
+      </div>
     </div>
   </div>
   <div class="row upper-bio-bg bg-gradient">
@@ -24,13 +32,30 @@
 <script>
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default {
   setup() {
+
+    onMounted(() => {
+    gsap.from(".fancy", {
+      duration: 2,
+      scale: 0.8,
+      opacity: 0,
+      ease: "elastic"
+    })
+    gsap.from(".social", {
+      duration: 2,
+      delay: 0.3,
+      scale: 0.8,
+      opacity: 0,
+      stagger: 0.2,
+      ease: "elastic"
+    })
+    })
     return {
-      
     }
   }
 }
