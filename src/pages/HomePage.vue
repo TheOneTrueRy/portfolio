c317be90452cb6344568367ff8ef37c1c1ef177f
Ryan Wittman <OfficiallyRy@outlook.com>
2023-04-07 09:31:25 -0600

    Added custom made bg by the love of my life, and diff placeholder imgs

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -36,13 +36,13 @@
     </div>
   </div>
   <div class="row upper-bio-bg elevation-4">
-    <div class="col-12 col-md-7 d-flex align-items-center justify-content-center bingbong">
+    <div class="col-12 col-md-6 d-flex align-items-center justify-content-center bingbong">
       <div class="p-4 rounded d-flex flex-column">
         <span class="fancy">Ryan Wittman</span>
-        <span class="subtitle">Designer & Developer</span>
+        <span class="subtitle text-light text-shadow">Designer & Developer</span>
       </div>
     </div>
-    <div class="col-5 g-0 d-none d-md-flex justify-content-end align-items-end h-100">
+    <div class="col-6 g-0 d-none d-md-flex justify-content-end align-items-end h-100">
       <img src="Actual-Ry.png" alt="Ryan Wittman Headshot" class="headshot">
     </div>
   </div>
@@ -50,22 +50,22 @@
     <div class="col-12">
       <div class="row right-to-left">
         <div class="col-4 g-0 elevation-2">
-          <img src="https://gaymingmag.com/wp-content/uploads/2020/11/kiryukazama.jpg" alt="" class="side-image">
+          <img src="https://media.timeout.com/images/105207533/image.jpg" alt="" class="side-image">
         </div>
-        <div class="col-8 px-5 py-3 d-flex flex-column justify-content-start align-items-center elevation-2">
-          <span class="fs-3 pt-1">Bing</span>
+        <div class="col-8 bg-dark border px-5 py-3 d-flex flex-column justify-content-start align-items-center elevation-2">
+          <span class="fs-3 pt-1">Coding Experience</span>
           <span class="pt-4 px-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem rerum veniam numquam eum? Nobis quam vero modi! Quis accusantium in impedit, ducimus dignissimos repudiandae sapiente et alias aliquid. Deleniti, nemo.</span>
         </div>
       </div>
     </div>
     <div class="col-12">
       <div class="row left-to-right">
-        <div class="col-8 px-5 py-3 d-flex flex-column justify-content-start align-items-center elevation-2">
-          <span class="fs-3 pt-1">Bong</span>
+        <div class="col-8 bg-dark border px-5 py-3 d-flex flex-column justify-content-start align-items-center elevation-2">
+          <span class="fs-3 pt-1">Personal Life</span>
           <span class="pt-4 px-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem rerum veniam numquam eum? Nobis quam vero modi! Quis accusantium in impedit, ducimus dignissimos repudiandae sapiente et alias aliquid. Deleniti, nemo.</span>
         </div>
         <div class="col-4 g-0 elevation-2">
-          <img src="https://gaymingmag.com/wp-content/uploads/2020/11/kiryukazama.jpg" alt="" class="side-image">
+          <img src="https://static2.srcdn.com/wordpress/wp-content/uploads/2020/10/gamingpc3.jpg" alt="" class="side-image">
         </div>
       </div>
     </div>
@@ -177,8 +177,10 @@ export default {
 
   .upper-bio-bg{
     height: 45vh;
-    background: rgb(48,44,44);
-    background: linear-gradient(90deg, rgba(48,44,44,1) 0%, rgba(255,255,255,1) 100%);
+    // background: rgb(48,44,44);
+    // background: linear-gradient(90deg, rgba(48,44,44,1) 0%, rgba(255,255,255,1) 100%);
+    background-image: url("BackgroundByCy3.png");
+    background-size: cover;
   }
 
   .headshot{
