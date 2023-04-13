59674f41ad646b7bd27d9e42c302bf88a088dea6
Ryan Wittman <OfficiallyRy@outlook.com>
2023-04-12 18:04:12 -0600

    Why it no work

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,12 @@
 </template>
 
 <script>
-import { computed } from 'vue'
-import { AppState } from './AppState'
-import Navbar from './components/Navbar.vue'
 
 export default {
   setup() {
     return {
-      appState: computed(() => AppState)
     }
-  },
-  components: { Navbar }
+  }
 }
 </script>
 <style lang="scss">
